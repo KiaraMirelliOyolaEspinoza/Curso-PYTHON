030811cd6d5aad814858c5f2e2223857db7a41aa
KiaraOyola <koyolae@unprg.edu.pe>
2021-03-01 01:22:15 -0500

    Agregando archivos básicos

---
+++ multiplicación.py
@@ -0,0 +1,6 @@
+a = input("Ingrese un número entero:")
+b = input("Ingrese otro número entero:")
+a = int(a)
+b = int(b)
+c = a * b
+print("la multiplicación es: ", c)
---
+++ suma.py
@@ -0,0 +1,6 @@
+a = input("Ingrese un número:")
+b = input("Ingrese otro número:")
+a = int(a)
+b = int(b)
+c = a + b
+print(" La suma de los números ingresados es:", c)
